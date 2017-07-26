7faa730c16a1881c704af25fa058e389a4dff0f0
Harvinder Power <harvinder.power@gmail.com>
2017-07-26 18:55:45 +0100

    Sentiment graphed against time
    Allows for tracking of trend in real-time

--- sentiment.py
+++ sentiment.py
@@ -1,21 +1,16 @@
 import tweepy
 from textblob import TextBlob
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
 from tweepy import Stream
-
+import numpy as np
+import matplotlib.pyplot as plt
 from tweepy.streaming import StreamListener
+from datetime import datetime
 
-plotly.tools.set_credentials_file(username='XXXX', api_key='XXXX')
+consumer_key = "XXX"
+consumer_secret = "XXX"
 
-
-#consumer and access key obtained through request at apps.twitter.com
-consumer_key = "XXXX"
-consumer_secret = "XXXX"
-
-access_token = "XXXX"
-access_token_secret = "XXXX" 
+access_token = "XXX"
+access_token_secret = "XXX" 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -26,15 +21,18 @@ class listener(StreamListener):
     def __init__(self):
         self.tweet_count = 0
         self.sum_polarity = 0
+        self.all_data = []
+        self.time_stamps = []
         
     def on_data(self, data):
         #tweet_count = int()
         #sum_polarity = int()
         
-        #print data
+        #print tweet data
         pure_tweet = data.split(',"text":"')[1].split(',"source"')[0]
         print pure_tweet
         
+        #print polarity of tweet
         polarity = TextBlob(pure_tweet).sentiment.polarity
         print "Polarity = ", polarity
         
@@ -46,12 +44,27 @@ class listener(StreamListener):
         print "sum polarity = ", self.sum_polarity
         self.sum_polarity = self.sum_polarity + polarity 
     
-        #if tweet_count > 0:
+        #calculate and print average polarity for tweets so far
         avg_polarity = self.sum_polarity/self.tweet_count
         print "Average Polarity =", avg_polarity
-        
-        return True
     
+        #creating a list of all polarity values
+        self.all_data.append(avg_polarity)
+        #print "All data list = ", self.all_data
+        
+        #generating time stamps for extraction times (though this should be the time of time ideally)
+        self.time_stamps.append(datetime.now())
+        #print "Time stamps = ", self.time_stamps
+        
+        #plotting the graph
+        plt.plot(self.time_stamps, self.all_data, 'ro')
+        plt.xlabel('Time')
+        plt.ylabel('Polarity of Sentiment')
+        plt.title('Sentiment of tweets against time')
+        plt.draw()
+        #if self.tweet_count % 10 == 0:
+        plt.show()
+        
     def on_error(self, data):
         print status
         
